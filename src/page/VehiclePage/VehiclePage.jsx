c10490f69dcd4b106b958665a985b4f2f6389b0e
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-09-11 02:37:05 +0300

    198

--- src/page/VehiclePage/VehiclePage.jsx
+++ src/page/VehiclePage/VehiclePage.jsx
@@ -4,12 +4,12 @@ import { selectorIsLoading } from "../../redux/catalog/selectors";
 import { useEffect, useRef } from "react";
 import { fetchVehicleById } from "../../redux/catalog/operations";
 // import VehiclePhoto from "../../components/VehiclePhoto/VehiclePhoto";
-import VehicleTitle from "../../components/VehicleTitle/VehicleTitle";
+// import VehicleTitle from "../../components/VehicleTitle/VehicleTitle";
 import Loader from "../../components/Loader/Loader";
 import css from "./VehiclePage.module.css";
 import { IoMdArrowBack } from "react-icons/io";
 import { NavLink } from "react-router-dom";
-import VehicleDescription from "../../components/VehicleDescription/VehicleDescription";
+// import VehicleDescription from "../../components/VehicleDescription/VehicleDescription";
 
 export default function VehiclePage() {
   const { id } = useParams();
@@ -42,11 +42,11 @@ export default function VehiclePage() {
         <IoMdArrowBack size={13} />
         Go back
       </NavLink>
-      <div className={css.contInfo}>
+      {/* <div className={css.contInfo}>
         <VehicleTitle />
-        {/* <VehiclePhoto /> */}
+        <VehiclePhoto />
       </div>
-      <VehicleDescription />
+      <VehicleDescription /> */}
     </div>
   );
 }
