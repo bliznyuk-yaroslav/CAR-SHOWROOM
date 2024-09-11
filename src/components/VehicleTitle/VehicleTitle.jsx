9d7b1f66fc7cf870b6b2502b230cf1db0ba22992
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-09-11 09:32:46 +0300

    31

--- src/components/VehicleTitle/VehicleTitle.jsx
+++ src/components/VehicleTitle/VehicleTitle.jsx
@@ -1,30 +1,40 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectVehicle } from "../../redux/catalog/selectors";
 import css from "./VehicleTitle.module.css";
 import { FaStar } from "react-icons/fa6";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchVehicleById } from "../../redux/catalog/operations";
 
 export default function VehicleTitle() {
+  const { id } = useParams();
+  const dispatch = useDispatch();
   const data = useSelector(selectVehicle);
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchVehicleById(id));
+    }
+  }, [id, dispatch]);
 
   return (
-        <div className={css.container}>
-          <h2 className={css.title}>
-            {data.brand}: {data.title}
-          </h2>
-          <div className={css.rating}>
-            <span className={css.text}>
-              <div className={css.star}>
-                <FaStar className={css.icon} />
-              </div>
-              <span className={css.reviews}>{`${data.rating}( Reviews)`}</span>
-            </span>
+    <div className={css.container}>
+      <h2 className={css.title}>
+        {data.brand}: {data.title}
+      </h2>
+      <div className={css.rating}>
+        <span className={css.text}>
+          <div className={css.star}>
+            <FaStar className={css.icon} />
           </div>
-          <h4>Price:</h4>
-          <p className={css.price}>$ {data.price}</p>
-          <div>
-            <h3>Description:</h3>
-            <p>{data.description}</p>
-          </div>
-        </div>
+          <span className={css.reviews}>{`${data.rating}( Reviews)`}</span>
+        </span>
+      </div>
+      <h4>Price:</h4>
+      <p className={css.price}>$ {data.price}</p>
+      <div>
+        <h3>Description:</h3>
+        <p>{data.description}</p>
+      </div>
+    </div>
   );
 }
