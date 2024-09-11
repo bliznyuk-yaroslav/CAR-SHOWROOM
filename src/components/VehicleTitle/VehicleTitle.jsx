9f425efceabdae13f7ad5898db93eb1d9e516ccb
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-09-11 09:38:10 +0300

    234

--- src/components/VehicleTitle/VehicleTitle.jsx
+++ src/components/VehicleTitle/VehicleTitle.jsx
@@ -1,28 +1,14 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectVehicle } from "../../redux/catalog/selectors";
 import css from "./VehicleTitle.module.css";
 import { FaStar } from "react-icons/fa6";
-import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-import { fetchVehicleById } from "../../redux/catalog/operations";
-import Loader from "../Loader/Loader";
 
 export default function VehicleTitle() {
-  const { id } = useParams();
-  const dispatch = useDispatch();
   const data = useSelector(selectVehicle);
-  useEffect(() => {
-    if (id) {
-      dispatch(fetchVehicleById(id));
-    }
-  }, [id, dispatch]);
-  if (!data) {
-    return <Loader />;
-  }
 
   return (
     <div className={css.container}>
-      <h2 className={css.title}>
+      {/* <h2 className={css.title}>
         {data.brand}: {data.title}
       </h2>
       <div className={css.rating}>
@@ -34,7 +20,7 @@ export default function VehicleTitle() {
         </span>
       </div>
       <h4>Price:</h4>
-      <p className={css.price}>$ {data.price}</p>
+      <p className={css.price}>$ {data.price}</p> */}
       <div>
         <h3>Description:</h3>
         <p>{data.description}</p>
