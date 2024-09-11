5e5bc8526b904f25750b59f164edb38e1b5bdb08
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-09-11 09:22:46 +0300

    41424

--- src/components/VehicleTitle/VehicleTitle.jsx
+++ src/components/VehicleTitle/VehicleTitle.jsx
@@ -8,25 +8,25 @@ export default function VehicleTitle() {
 
   return (
     <>
-        <div className={css.container}>
-          <h2 className={css.title}>
-            {data.brand}: {data.title}
-          </h2>
-          <div className={css.rating}>
-            <span className={css.text}>
-              <div className={css.star}>
-                <FaStar/>
-              </div>
-              <span className={css.reviews}>{`${data.rating}( Reviews)`}</span>
-            </span>
-          </div>
-          <h4>Price:</h4>
-          <p className={css.price}>$ {data.price}</p>
-          <div>
-            <h3>Description:</h3>
-            <p>{data.description}</p>
-          </div>
+      <div className={css.container}>
+        <h2 className={css.title}>
+          {data.brand}: {data.title}
+        </h2>
+        <div className={css.rating}>
+          <span className={css.text}>
+            <div className={css.star}>
+              <FaStar className={css.icon} />
+            </div>
+            <span className={css.reviews}>{`${data.rating}(Reviews)`}</span>
+          </span>
         </div>
+        <h4>Price:</h4>
+        <p className={css.price}>$ {data.price}</p>
+        <div>
+          <h3>Description:</h3>
+          <p>{data.description}</p>
+        </div>
+      </div>
     </>
   );
 }
